f6bce19b49dcabc8b65e00278b39c3bbd2fc0c31
lwk <1831563277@qq.com>
2021-04-07 12:51:38 +0800

    修复分配权限

--- src/views/power/Roles.vue
+++ src/views/power/Roles.vue
@@ -233,7 +233,7 @@ export default {
         this.$message.error('分配权限失败！')
       } else {
         this.$message.success('分配权限成功！')
-        this.getRolesList()
+        this.getRoleList();
         this.setRightDialogVisible = false
       }
 
